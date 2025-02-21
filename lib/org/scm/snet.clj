7e92cc8c86a22ffbaf9dd4ca9ed98451a44da167
zhonghua <secondsquare@gmail.com>
2025-02-21 17:47:10 +0800

    add functionality that server's nio socket can read data from client according the len+bytes format

--- lib/org/scm/snet.clj
+++ lib/org/scm/snet.clj
@@ -1,32 +1,117 @@
 (ns org.scm.snet)
 
+(require '[clojure.string :as str])
+
 (import '[java.nio.channels Selector]
         '[java.nio.channels ServerSocketChannel]
         '[java.nio.channels SelectionKey]
         '[java.nio ByteBuffer]
-        '[java.net InetSocketAddress])
+        '[java.net InetSocketAddress]
+        '[java.util.concurrent ConcurrentLinkedQueue])
 
 (def conn-id (atom 1))
 
-(defn handle-accept [selector srvchannel fn-new-conn]
+(defn new-client [channel]
+  {:id (swap! conn-id inc)
+   :addr (.getRemoteAddress channel)
+   :channel channel
+   :read-state (atom 1) ;; 1 : read len 2 : read body
+   :continue-read (atom true)
+   :len-buf (ByteBuffer/allocate 4)
+   :msg-buf (atom nil)
+   :inqueue (ConcurrentLinkedQueue.)
+   :outqueue (ConcurrentLinkedQueue.)
+   :lastrecvtime (atom 0)
+   :lastsendtime (atom 0)})
+
+(defn client-id [inst]
+  (str "[:id " (:id inst) " :addr " (:addr inst) "]"))
+;;
+;;(defn err-throw [inst & args]
+;;  (let [e-arg (cons (client-id inst) args)]
+;;  (apply println e-arg)
+;;  (throw (Exception. (str/join " " e-arg)))))
+;;
+;;(defmacro assert-throw [stat inst & args]
+;;  `(when (not ~stat)
+;;     (err-throw ~inst ~@args)))
+
+(defn handle-accept [selector key srvchannel fn-new-conn]
   (let [client (.accept srvchannel)]
     (.configureBlocking client false)
-    (.register client selector SelectionKey/OP_READ)
-    (fn-new-conn (swap! conn-id inc) client
-                 (str (-> client .socket .getInetAddress)
-                      ":"
-                      (-> client .socket .getPort)))))
+    (let [inst (new-client client)]
+      (.register client selector SelectionKey/OP_READ inst)
+      (fn-new-conn inst))))
 
-(defn handle-read [selector key]
+(defn read-len [key channel inst fn-err]
+  (let [len-buf (:len-buf inst)
+        rcount (.read channel len-buf)]
+    (cond (< rcount 0)
+          (do (println "read-len count < 0" rcount (client-id inst))
+              (reset! (:continue-read inst) false)
+              (.cancel key)
+              (fn-err inst)
+              )
+
+          (not (.hasRemaining len-buf))
+          (do (reset! (:continue-read inst) true)
+              (.flip len-buf)
+              (let [len (.getInt len-buf)]
+                (cond (<= len 0)
+                      (do (reset! (:continue-read inst) false)
+                          (println "msg len less than or equal 0" len (client-id inst))
+                          (.cancel key)
+                          (fn-err inst))
+
+                      (>= len 65535)
+                      (do (reset! (:continue-read inst) false)
+                          (println "msg len too large" len (client-id inst))
+                          (.cancel key)
+                          (fn-err inst))
+
+                      :else
+                      (do (reset! (:msg-buf inst) (ByteBuffer/allocate len))
+                          (reset! (:read-state inst) 2))
+                      ))
+              )
+
+          :else (reset! (:continue-read inst) false)
+          )))
+
+(defn read-body [key channel inst fn-err]
+  (let [msg-buf @(:msg-buf inst)
+        rcount (.read channel msg-buf)]
+    (if (< rcount 0)
+      (do (reset! (:continue-read inst) false)
+          (println "read-body count < 0" rcount (client-id inst))
+          (.cancel key)
+          (fn-err inst))
+
+      (when (not (.hasRemaining msg-buf))
+        (reset! (:continue-read inst) true)
+        (.flip msg-buf)
+        (.flip (:len-buf inst))
+        (reset! (:read-state inst) 1)
+        (reset! (:msg-buf inst) nil)
+        (println "recved" (String. (.array msg-buf)))
+        ))))
+
+(defn handle-read [selector key fn-err]
   (let [client (.channel key)
-        buffer (ByteBuffer/allocate 256)
-        rcount (.read client buffer)]
-    ))
+        inst (.attachment key)]
+    (while @(:continue-read inst)
+      (case @(:read-state inst)
+        1 (read-len key client inst fn-err)
+        2 (read-body key client inst fn-err)
+
+        (do (reset! (:continue-read inst) false)
+            (println "internal error read-state" (:read-state inst) (client-id inst))
+            )))))
 
 (defn handle-write [selector key]
   (let [client (.channel key)]))
 
-(defn handle-net-events [selector srvchannel fn-new-conn]
+(defn handle-net-events [selector srvchannel fn-new-conn fn-err]
   (while true
     (.select selector)
 
@@ -36,13 +121,18 @@
       (while (.hasNext iter)
         (let [key (.next iter)]
           (.remove iter)
-          (cond (.isAcceptable key) (handle-accept selector srvchannel fn-new-conn)
-                (.isReadable key) (handle-read selector key)
-                (.isWritable key) (handle-write selector key)))))
+          (try
+            (cond (.isAcceptable key) (handle-accept selector key srvchannel fn-new-conn)
+                  (.isReadable key) (handle-read selector key fn-err)
+                  (.isWritable key) (handle-write selector key))
+            (catch Exception e
+              (println "exception" e)
+              (.cancel key)
+              (when-let [inst (.attachment key)]
+                (fn-err inst)))))))
+    (Thread/sleep 30)))
 
-    ))
-
-(defn init-server [port fn-new-conn]
+(defn init-server [port fn-new-conn fn-err]
   (let [selector (Selector/open)
         srvchannel (ServerSocketChannel/open)]
     (.configureBlocking srvchannel false)
@@ -53,10 +143,13 @@
     (.register srvchannel selector SelectionKey/OP_ACCEPT)
 
     (-> (Thread. (fn [] (handle-net-events selector srvchannel
-                                           fn-new-conn)))
+                                           fn-new-conn fn-err)))
         .start)))
 
 (defn -main []
   (init-server 1666
-               (fn [id client-sock-channel addr]
-                 (println "new conn" id "addr" addr))))
+               (fn [client]
+                 (println "new conn " (client-id client)))
+               (fn [client]
+                 (println "error conn" (client-id client)))
+               ))
--- lib/org/scm/test.clj
+++ lib/org/scm/test.clj
@@ -1,5 +1,8 @@
 (ns org.scm.test)
 
+(import '[java.nio.channels SocketChannel]
+        '[java.nio ByteBuffer]
+        '[java.net InetSocketAddress])
 
 (defn byte->bytes [bs offset val]
   (let [low1 (bit-and val 0x7F)
@@ -25,7 +28,33 @@
         (list (bit-or val (bit-shift-left val2 7))
               (+ 1 c))))))
 
+(defn client [host port]
+  (let [channel (SocketChannel/open)
+        msg (.getBytes "Hello Server")
+        len (ByteBuffer/allocate 4)
+        buf (ByteBuffer/allocate (alength msg))]
+
+    (println "connecting server" host port)
+    (.configureBlocking channel false)
+    (.connect channel (InetSocketAddress. host port))
+    (while (not (.finishConnect channel))
+      (Thread/sleep 500))
+    (println "connect server success " host port)
+
+    (.putInt len (alength msg))
+    (.put buf msg)
+
+    (.write channel (.flip len))
+    (.write channel (.flip buf))
+    (println "write success")
+    (Thread/sleep 500000000)
+    ))
+
 (defn -main []
-  (let [tbs (bytes (byte-array 10))]
-    (println (uint->bytes tbs 0 566))
-    (println (bytes->uint tbs 0))))
+;;  (let [tbs (bytes (byte-array 10))]
+;;    (println (uint->bytes tbs 0 566))
+;;    (println (bytes->uint tbs 0)))
+
+  (client "127.0.0.1" 1666)
+
+  )
